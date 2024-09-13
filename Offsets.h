e46dbdbcb285fc67a1d315f0bd6f95f7824e6fad
nb1027 <154864779+nb1027@users.noreply.github.com>
2024-09-13 16:41:08 -0500

    Update Offsets.h

--- Offsets.h
+++ Offsets.h
@@ -4,21 +4,21 @@
 
 namespace Offsets
 {
-	inline uint64_t UWorld = 0xA002150;
-	inline uint64_t OwningGameInstance = 0x358;
-	inline uint64_t PersistentLevel = 0x1D0;
+	inline uint64_t UWorld = 0x9E25120;
+	inline uint64_t OwningGameInstance = 0x478;
+	inline uint64_t PersistentLevel = 0x2F0;
 	inline uint64_t AActors = 0x98;
-	inline uint64_t AActorsCount = 0x0A0;
+	inline uint64_t AActorsCount = 0xA0;
 	inline uint64_t LocalPlayers = 0x38;
 	inline uint64_t PlayerController = 0x30;
 	inline uint64_t RelativeLocation = 0x140;
-	inline uint64_t AcknowledgedPawn = 0x578;
-	inline uint64_t CameraManager = 0x590;
-	inline uint64_t CameraCache = 0x27F0;
-	inline uint64_t PlayerState = 0x4E0;
-	inline uint64_t Names = 0x5B8;
-	inline uint64_t RootComponent = 0x3C8;
-	inline uint64_t DescriptiveName = 0x10E0;
+	inline uint64_t AcknowledgedPawn = 0x580;
+	inline uint64_t CameraManager = 0x598;
+	inline uint64_t CameraCache = 0x2970;
+	inline uint64_t PlayerState = 0x4D0;
+	inline uint64_t Names = 0x5A0;
+	inline uint64_t RootComponent = 0x3D0;
+	inline uint64_t DescriptiveName = 0x1110;
 }
 
 namespace Globals
